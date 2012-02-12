8215c18ca0a350842bdc6efe69f4fc9496c7009d
Greyson Fischer <greyson@foosoft.us>
2012-02-12 16:30:40 -0500

    Added comments and a copyright notice

--- src/pam.cpp
+++ src/pam.cpp
@@ -1,6 +1,13 @@
-#define PAM_SM_AUTH
+/* pam_couchdb module */
+
+/*
+ * Written by Greyson Fischer <greyson@foosoft.us> 2012/01/01
+ */
 
 #include "curl.hpp"
+
+#define PAM_SM_AUTH
+
 #include <security/pam_modules.h>
 #include <security/_pam_macros.h>
 
@@ -9,7 +16,12 @@ extern "C"
    PAM_EXTERN int pam_sm_authenticate( pam_handle_t *, int, int, char const ** );
    PAM_EXTERN int pam_sm_setcred( pam_handle_t *, int, int, char const ** );
 }
-
+/**
+ * Overwrite possibly sensitive memory before free-ing it.
+ *
+ * @param  xx String containing possibly sensitive information
+ * @retval Always NULL (useful for reseting pointer in the same line)
+ */
 static char * _pam_delete( register char * xx )
 {
    _pam_overwrite( xx );
@@ -135,3 +147,40 @@ int pam_sm_setcred( pam_handle_t * pamh, int flags,
 {
    return PAM_SUCCESS;
 }
+
+/*
+ * Copyright (c) Greyson Fischer <greyson@foosoft.us>, 2012 All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice and the entire permission notice in its entirety,
+ *    including the disclaimer of warranties.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * ALTERNATIVELY, this product may be distributed under the terms of
+ * the GNU Public License, in which case the provisions of the GPL are
+ * required INSTEAD OF the above restrictions.  (This clause is
+ * necessary due to a potential bad interaction between the GPL and
+ * the restrictions contained in a BSD-style copyright.)
+ *
+ * THIS SOFTWARE IS PROVIDED `AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
